07c4855df112d7541c1c7d16fc59ebd78c8bfb11
Rodrigo Travessa <rodrigosigiliao@outlook.com>
2023-07-14 17:55:25 -0300

    ConnectionStrigns Fixed

--- IatPrepExam/Program.cs
+++ IatPrepExam/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionConnection")));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
