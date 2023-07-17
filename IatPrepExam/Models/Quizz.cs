9b931d1e6da039e946e5c6aaeff9808beafc85fc
Rodrigo Travessa <rodrigosigiliao@outlook.com>
2023-07-17 10:20:31 -0300

    Melhorias no Cabeçalho e Display info de ID da questão

--- IatPrepExam/Models/Quizz.cs
+++ IatPrepExam/Models/Quizz.cs
@@ -10,13 +10,16 @@ namespace IatPrepExam.Models
         public DateTime StartedAt { get; set; } = DateTime.Now;
         public DateTime FinishedAt { get; set; }
         public List<Question> Questions { get; set; } = new ();
+        [DisplayName("Pontuação")]
         public double Score { get; set; }        
         public List<Answer> Answers { get; set; } = new ();
         [DisplayName("Nome do Estudante")]
         public string? NameOfStudent { get; set; }
         [DisplayName("Número de Questões")]
         public int NumberOfQuestions { get; set; }
+        [DisplayName("% do Total de Pontos")]
         public double ScoreRatio { get; set; }
+
         public int Rights { get; set; }
     }
 }
